01651c263ca4d69a9a622b488d727ddd5e8b4fac
BabyPowderr <167431437+BabyPowderr@users.noreply.github.com>
2025-09-13 21:59:19 +0800

    Add files via upload

---
+++ 商城/pages/index/index.js
@@ -0,0 +1,69 @@
+var WxAutoImage = require('../../js/wxAutoImageCal.js');
+var app = getApp();
+
+Page({
+    data: {
+        imgUrls: [
+            '../../image/swiper1.jpg',
+            '../../image/swiper1.jpg',
+            '../../image/swiper1.jpg'
+        ],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        interval: 3000,
+        duration: 1200,
+        iconArray: [
+            {
+                "iconUrl": '../../image/icon-qiandao.png',
+                "iconText": '签到'
+            },
+            {
+                "iconUrl": '../../image/icon-fujin.png',
+                "iconText": '考试宝典'
+            },
+            {
+                "iconUrl": '../../image/icon-zhanhui.png',
+                "iconText": '商城'
+            },
+            {
+                "iconUrl": '../../image/icon-fuli.png',
+                "iconText": '福利'
+            },
+            {
+                "iconUrl": '../../image/icon-muma.png',
+                "iconText": '玩乐'
+            },
+            {
+                "iconUrl": '../../image/icon-xingxing.png',
+                "iconText": '湾区美食'
+            },
+            {
+                "iconUrl": '../../image/icon-tiyu.png',
+                "iconText": '体育音乐'
+            },
+            {
+                "iconUrl": '../../image/icon-qinzi.png',
+                "iconText": '汽车周边'
+            }
+        ],
+        itemArray: [
+            {
+                "itemUrl": '../../image/huaju.jpeg',
+                "itemText": '11月20日话剧《风声》'
+            },
+            {
+                "itemUrl": '../../image/huaju.jpeg',
+                "itemText": '11月20日话剧《原野》'
+            },
+            {
+                "itemUrl": '../../image/huaju.jpeg',
+                "itemText": '11月28日“夜店”演唱会'
+            },
+        ]
+    },
+    cusImageLoad: function(e){
+        var that = this;
+        that.setData(WxAutoImage.wxAutoImageCal(e));
+    }
+})
